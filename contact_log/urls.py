257527ab423ef0de6bbea6433ae7634d9a04c380
andreilemisz <andreilemisz@gmail.com>
2024-08-26 10:22:48 -0300

    Created the Search buttom in the Contact Log page, as well as added pagination with a limit of 6 contacts per page.

--- contact_log/urls.py
+++ contact_log/urls.py
@@ -6,8 +6,9 @@ app_name = "contact_log"
 
 urlpatterns = [
     path('<int:contact_id>/', contact_log_views.contact, name='contact'), 
+    path("search/", contact_log_views.contact_log_search, name="search"),
     path("", contact_log_views.contact_log_startingpage, name="contact_log_startingpage"),
-]
+    ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- contact_log/views.py
+++ contact_log/views.py
@@ -1,11 +1,46 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
+from django.core.paginator import Paginator
 from contact_log.models import Contact
 
 def contact_log_startingpage(request):
     
-    contacts = Contact.objects.all().order_by("id")[:10]
+    contacts = Contact.objects.all().order_by("id")
+    paginator = Paginator(contacts, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        "contacts" : contacts,
+        "page_obj" : page_obj,
+        "site_title": "Contacts",
+    }
+    
+    return render(request, 'contact_log/homepage.html', context)
+
+def contact_log_search(request):
+    
+    search_value = request.GET.get("q", "").strip()
+    
+    if search_value == "":
+        return redirect("contact_log:contact_log_startingpage")
+    
+    contacts = Contact.objects\
+            .filter(
+            Q(name__icontains=search_value) |
+            Q(rank__name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by("id")
+        
+    paginator = Paginator(contacts, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+        
+    context = {
+        "page_obj" : page_obj,
+        "site_title": "Contacts",
+        'search_value': search_value,
     }
     
     return render(request, 'contact_log/homepage.html', context)
@@ -17,6 +52,7 @@ def contact(request, contact_id):
 
     context = {
         'contact': single_contact,
+        "site_title": single_contact.name
     }
 
     return render(
